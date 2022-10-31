c9f3ce7b8afe3946a113813a17f68e5025f24324
zksyncenjoyer <117069613+zksyncenjoyer@users.noreply.github.com>
2022-10-31 01:08:18 -0700

    Update preliminary_calc.py

--- .github/workflows/preliminary_calc.py
+++ .github/workflows/preliminary_calc.py
@@ -8,7 +8,11 @@ fees_in_BPS = int(input("Input fees in BPS (if 30 bps, enter 30)\n"))
 risk_premia = int(input("Input annual risk premia (if you don't know enter 5 for 5%)\n"))
 hurdle = a_LVR + risk_premia
 income = fees_in_BPS * volume_as_percent_TVL * 365 / 10000
+print('Opportunity Cost: {:.1%}\n'.format(hurdle/100))
+print('Promixate Pool Fees: {:.1%}\n'.format(income/100))
 if income > hurdle:
     print("LPing may be profitable")
 else:
-    print("LPing is definitely not profitable")
+    gap = (hurdle - income) / 100
+    print("LPing is definitely not profitable\n")
+    print('Shitcoin reward apy to breakeven is: {:.1%}\n'.format(gap))
